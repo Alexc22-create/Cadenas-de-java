781da76cb846fc995a504f5a2471b05be813dbb9
Alexc22-create <20243rd035@utez.edu.mx>
2025-05-08 11:54:57 -0600

    Se le agregaron los apellidos del usuario

--- Cadena.java
+++ Cadena.java
@@ -5,11 +5,15 @@ public class Cadena{
 
         Scanner almacenar = new Scanner(System.in);
         String nombre = "Aljandro";
-        System.out.println("Nombre: " + nombre);
+        System.out.println("Mi nombre es: " + nombre);
         System.out.println("Escribe tu nombre: ");
         nombre = almacenar.nextLine();
+        System.out.println("Escribe tu apellido Paterno: ");
+        String apellodo_Paterno = almacenar.nextLine();
+        System.out.println("Escribe tu apellido Materno: ");
+        String apellido_Materno = almacenar.nextLine();
 
-        System.out.println("Tu nombre es: "+ nombre);
+        System.out.println("Tu nombre es: "+ nombre + " "+apellodo_Paterno + " " +apellido_Materno);
 
 
     }
